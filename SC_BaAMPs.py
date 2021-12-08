592060d76d7bfb5b02509fa6c57ae62acd0b5b85
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2021-12-08 11:10:18 -0300

    Basic setup with first line of table read

--- SC_BaAMPs.py
+++ SC_BaAMPs.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 DRIVER_PATH='/usr/local/bin/chromedriver'
 
@@ -11,5 +12,12 @@ driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 target = 'http://www.baamps.it/experimentlist'
 
 driver.get(target)
-print(driver.title)
-print(driver.current_url)
+
+rows = driver.find_elements_by_class_name('row0')
+
+firstLine = rows[0]
+
+elements = firstLine.find_elements(By.TAG_NAME, 'td')
+
+for e in elements:
+    print(e.text)
