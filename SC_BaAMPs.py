b0d11a350df26c90f0b9a22adc2dc92b30589038
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2021-12-09 09:17:57 -0300

    lint warnings fixed + function created to get_experiment

--- SC_BaAMPs.py
+++ SC_BaAMPs.py
@@ -2,16 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-DRIVER_PATH='/usr/local/bin/chromedriver'
+DRIVER_PATH = '/usr/local/bin/chromedriver'
 
 options = Options()
 options.headless = True
 options.add_argument("--windows-size=1920,1200")
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 
-target = 'http://www.baamps.it/experimentlist'
+URL = 'http://www.baamps.it/experimentlist'
 
-driver.get(target)
+driver.get(URL)
 
 rows = driver.find_elements_by_class_name('row0')
 
@@ -19,5 +19,12 @@ firstLine = rows[0]
 
 elements = firstLine.find_elements(By.TAG_NAME, 'td')
 
-for e in elements:
-    print(e.text)
+
+def get_experiment(elements):
+    experiment = []
+    for e in elements:
+        experiment.append(e.text)
+    return experiment
+
+
+print(get_experiment(elements))
